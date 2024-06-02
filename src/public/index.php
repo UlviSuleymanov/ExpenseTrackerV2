de6b545e525699b0146387cba3dce4261562b174
Ulvi <ulvi.suleymanov.yusif@gmail.com>
2024-06-02 17:40:30 +0400

    Testing PHP without AutoLoader

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,17 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 require_once '../Transactions.php';
 require_once './Stripe/Transactions.php';
+
 use Stripe\Transactions;
 use Public\Transactions as PublicTransaction;
-$transactions1 = new Transactions();
-$transactions = new PublicTransaction(100,"Transaction 1");
 
-$transactions ->addTax(8);
-$transactions ->addDiscount(10);
+$transactions1 = new Transactions();
+$transactions = new PublicTransaction(100, "Transaction 1");
+
+$transactions->addTax(8);
+$transactions->addDiscount(10);
 
 var_dump($transactions->amount);
 echo '<pre/>';
