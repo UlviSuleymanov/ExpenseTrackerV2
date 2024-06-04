e17c9c6c4c6672bc5343c6e81888aeb21d64cc66
Ulvi <ulvi.suleymanov.yusif@gmail.com>
2024-06-04 14:19:10 +0400

    Using & testing  custom autoload with spl registe

--- src/public/Transactions.php
+++ src/public/Transactions.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-namespace Public;
 class Transactions{
  public float $amount;
  public string $description;
--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,17 @@
 <?php
 
 declare(strict_types=1);
-require_once '../Transactions.php';
-require_once './Stripe/Transactions.php';
+//require_once '../Transactions.php';
+//require_once './Stripe/Transactions.php';
 
 use Stripe\Transactions;
-use Public\Transactions as PublicTransaction;
+use Transactions as PublicTransaction;
+
+//custom autoloading with spl register function.
+spl_autoload_register(function ($class){
+    $path = __DIR__ . '/' . lcfirst(str_replace('\\','/',$class)) . '.php';
+    require $path;
+});
 
 $transactions1 = new Transactions();
 $transactions = new PublicTransaction(100, "Transaction 1");
